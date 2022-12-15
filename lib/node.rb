f7441b24aeb47b219d25d5547cf7742c5c67897b
clipporion <marcusberger008@gmx.net>
2022-12-15 19:15:24 +0100

    Create Node class and tests
    I created a node class to represent a single field for the connect4 game
    which has it's place on the field stored as it's value as well as an
    instance variable to store the player's symbol.
    It also stores each neighbor as an instance variable named after the
    location on the field in relation to the node itself, which I will
    utilize to check the winning condition of the game.

---
+++ lib/node.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+# In this class I will store a node containing a value,
+# as well as a symbol in case the node is chosen to contain one
+# and also all it's neighbor nodes available on the field.
+class Node
+  attr_reader :value, :symbol, :left, :leftdown, :leftup,
+              :up, :down, :right, :rightdown, :rightup
+
+  def initialize(value)
+    @value = value
+    @symbol = nil
+    @leftup = add_neighbor(value, [-1, 1])
+    @up = add_neighbor(value, [0, 1])
+    @rightup = add_neighbor(value, [1, 1])
+    @left = add_neighbor(value, [-1, 0])
+    @right = add_neighbor(value, [1, 0])
+    @leftdown = add_neighbor(value, [-1, -1])
+    @down = add_neighbor(value, [0, -1])
+    @rightdown = add_neighbor(value, [1, -1])
+  end
+
+  def add_neighbor(value, move)
+    row = value[0] + move[0]
+    line = value[1] + move[1]
+    [row, line] if (1..7).include?(row) && (1..6).include?(line)
+  end
+
+  def add_symbol(symbol)
+    @symbol = symbol
+  end
+end
---
+++ spec/node_spec.rb
@@ -0,0 +1,64 @@
+# frozen_string_literal: true
+
+require './lib/node'
+
+describe Node do
+  subject(:node) { described_class.new([2, 2]) }
+
+  describe '#initialize' do
+    it 'creates a new node with a specified value' do
+      value = node.value
+
+      expect(value).to eq([2, 2])
+    end
+
+    it 'creates a node with a symbol add to nil' do
+      symbol = node.symbol
+
+      expect(symbol).to be nil
+    end
+
+    it 'creates 8 correct neighbors' do
+      leftup = node.leftup
+      up = node.up
+      rightup = node.rightup
+      left = node.left
+      right = node.right
+      leftdown = node.leftdown
+      down = node.down
+      rightdown = node.rightdown
+
+      expect(leftup).to eq([1, 3])
+      expect(up).to eq([2, 3])
+      expect(rightup).to eq([3, 3])
+      expect(left).to eq([1, 2])
+      expect(right).to eq([3, 2])
+      expect(leftdown).to eq([1, 1])
+      expect(down).to eq([2, 1])
+      expect(rightdown).to eq([3, 1])
+    end
+  end
+
+  describe '#add_neighbor' do
+    it 'creates a neighbor in case the field contains one' do
+      neighbor = node.add_neighbor([2, 2], [1, 1])
+
+      expect(neighbor).to eq([3, 3])
+    end
+
+    it 'returns nil if the field does not contain the neighbor' do
+      neighbor = node.add_neighbor([7, 6], [1, 1])
+
+      expect(neighbor).to be nil
+    end
+  end
+
+  describe '#add_symbol' do
+    it 'adds the symbol in case the node is chosen' do
+      node.add_symbol("\u25cf")
+      symbol = node.symbol
+
+      expect(symbol).to eq("\u25cf")
+    end
+  end
+end
